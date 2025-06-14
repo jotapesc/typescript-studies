6c97f8239725cf5013f5f471233ccffcab4d51b7
Akarui <minezicabr0@gmail.com>
2025-06-14 10:25:46 -0300

    refactor: redirect files to a new folder
    redirect the index.ts and style.ts to a new folder named "src"

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let names = "joão";
+let arr: string[];
+let obj: object = {}
+let float: number;
+
+let num: number; 
+
+//Any 
+let things;
+things = 12;
+things = "male";
+things = ["A", "B", "C", "D"];
+
