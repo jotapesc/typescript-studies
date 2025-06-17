3a6a99cbe814bf458d0a6b49328f7bee89d72d9d
João Pedro <jpsc081204@gmail.com>
2025-06-17 10:53:19 -0300

    feat: add arrays to the code
    learned the importance of types in arrays and how to do so

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,8 @@ things = 12;
 things = "male";
 things = ["A", "B", "C", "D"];
 
+//Array
+let values: number[] = [1, 2, 3, 4, 5];
+let listOfNames: string[] = ["James", "Belle", "Brutus", "Joseph", "Hens"];
+// values.push("Robert"); this will return an error "string cannot be assigned to a number"
+// listOfNames.push(6); this will return an error "number cannot be assigned to a string"
