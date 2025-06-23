1402377d4144ad38dcb48c3cccd7440290dc5ed3
João Pedro <jpsc081204@gmail.com>
2025-06-23 11:40:46 -0300

    feat: add exercise 1
    add an exercise about primitive types in typescript

---
+++ src/exercises/primitive-types-exercise.ts
@@ -0,0 +1,16 @@
+//Exercise: Declare variables with proper primitive type annotations. age as a number,name as a string, isActive as a boolean, etc.
+const systemUser: {
+  age: number;
+  firstName: string;
+  userEmail: string;
+  emailVerified: null;
+  havePet: boolean;
+  bloodType: undefined;
+} = {
+  age: 47,
+  firstName: "Bob",
+  userEmail: "bobawesomeemail@email.com",
+  emailVerified: null,
+  havePet: true,
+  bloodType: undefined,
+};
