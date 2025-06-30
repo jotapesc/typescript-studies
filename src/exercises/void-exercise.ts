483234dde534e425da4adc4b3041162a7e509b6b
João Pedro <jpsc081204@gmail.com>
2025-06-30 09:55:42 -0300

    feat: add exercise 5
    add an exercise about the void type in typescript

---
+++ src/exercises/void-exercise.ts
@@ -0,0 +1,7 @@
+// Exercise: Write a function logMessage that takes a string parameter and returns void.
+
+function logMessage(message:string):void {
+    console.log(message);
+}
+
+logMessage(`Error: 418 "I'm a teapot"`);
