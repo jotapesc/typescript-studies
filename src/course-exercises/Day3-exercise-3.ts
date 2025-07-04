1996faf6dfb23d4c7708ebddc112ab712914f4d3
João Pedro <jpsc081204@gmail.com>
2025-07-04 11:56:26 -0300

    feat: add exercise 3 from the course

---
+++ src/course-exercises/Day3-exercise-3.ts
@@ -0,0 +1,10 @@
+// Day 3 - Exercise 3: correct the errors
+
+// let isDouglas: boolean
+
+// console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`)
+
+let isDouglas: boolean;
+isDouglas = true;
+
+console.log(`${isDouglas ? "Oh, Hi Douglas" : "Who are you?"}`);
