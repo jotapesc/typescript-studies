b0f4422dd250466deb83aab1e5139efd06bad704
João Pedro <jpsc081204@gmail.com>
2025-07-04 11:52:03 -0300

    feat: add exercise 2 from the course

---
+++ src/course-exercises/Day3-exercise-2.ts
@@ -0,0 +1,11 @@
+// Day 3 - Exercise 2: adapt to typescript
+
+// let pie
+// pie = 'blueberry'
+
+// console.log(`I like to eat ${pie}-flavored pie.`)
+
+let pie: string; //pie didn't have a type defined
+pie = "blueberry"
+
+console.log(`I like to eat ${pie}-flavored pie.`);
