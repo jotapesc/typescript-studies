91a8f162efea5a03ccf2a9a4418ecae913b85599
João Pedro <jpsc081204@gmail.com>
2025-07-09 13:48:44 -0300

    feat: add exercise 5 from the course

---
+++ src/course-exercises/day-7/Day7-exercise-5.ts
@@ -0,0 +1,13 @@
+// Day 7 - Exercise 5: correct the errors
+
+// function layEggs(quantity, color) {
+//     console.log(`You just laid ${quantity} ${color} eggs. Good job!`);
+//   }
+
+//   layEggs();
+
+function layEggs(quantity: number, color: string): void {
+  console.log(`You just laid ${quantity} ${color} eggs. Good job!`);
+}
+
+layEggs(10, "blue");
